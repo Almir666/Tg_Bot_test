02b74a2614e593d9ab3ab7c3178f977ea4709880
Almir <crafteasy@yandex.ru>
2022-09-04 19:34:31 +0300

    немного поизучал создание бота, будуизучать дальше

--- FirstBot/First_bot.py
+++ FirstBot/First_bot.py
@@ -1,11 +1,26 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('5412859207:AAHF165SttlmMiD9o-pyfqh9SKtAN7_9k4Q')
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Привет, <b>{message.from_user.first_name}</b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
+
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Come on, let's go!", url="https://xn--b1aew.xn--p1ai/"))
+    bot.send_message(message.chat.id, 'Посетить Gey website', reply_markup=markup)
+
+@bot.message_handler(commands=['help'])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=1)
+    website = types.KeyboardButton("Come one, let's go!")
+    
+    markup.add(website)
+    bot.send_message(message.chat.id, 'Посетить Gey website', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
@@ -22,7 +37,6 @@ def get_user_text(message):
 @bot.message_handler(content_types=['photo'])  
 def get_user_photo(message):
     bot.send_message(message.chat.id, 'Ооо, СПАСИБО ОГРОМНОЕ! Но напомни ка мне, когдая тебя просил присылать мне фото или картинки, а????')
-
 
 
 
